95f56100ef217ff925b2c825762b77e6d46a8b32
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-03-21 14:58:15 -0700

    Updating to gog_media

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/arelate/vangogh_types
 
 go 1.16
 
-require github.com/arelate/gog_types v0.1.6-alpha
+require (
+	github.com/arelate/gog_types v0.1.6-alpha
+	github.com/arelate/gog_media v0.1.2-alpha
+)
 
-replace github.com/arelate/gog_types => ../gog_types
+replace (
+	github.com/arelate/gog_types => ../gog_types
+	github.com/arelate/gog_media => ../gog_media
+)
--- supportsMedia.go
+++ supportsMedia.go
@@ -1,22 +1,24 @@
 package vangogh_types
 
-import "github.com/arelate/gog_types"
+import (
+	"github.com/arelate/gog_media"
+)
 
 // unsupported is used instead of supported in similar cases to
 // avoid all, but one repetitive data
-var unsupporedMedia = map[ProductType][]gog_types.Media{
-	ApiProductsV2: {gog_types.Movie},
+var unsupportedMedia = map[ProductType][]gog_media.Media{
+	ApiProductsV2: {gog_media.Movie},
 }
 
-func SupportsMedia(pt ProductType, mt gog_types.Media) bool {
-	if !gog_types.ValidMedia(mt) {
+func SupportsMedia(pt ProductType, mt gog_media.Media) bool {
+	if !gog_media.Valid(mt) {
 		return false
 	}
 	if !ValidProductType(pt) {
 		return false
 	}
 
-	ums, ok := unsupporedMedia[pt]
+	ums, ok := unsupportedMedia[pt]
 	if !ok {
 		return true
 	}
