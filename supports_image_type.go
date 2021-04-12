50efd90808f9a49850ac7e229b45a34d600e5459
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-04-11 22:16:32 -0700

    Adding video (as image type)

--- supports_image_type.go
+++ supports_image_type.go
@@ -3,7 +3,11 @@ package vangogh_products
 import "github.com/arelate/vangogh_images"
 
 var supportedImageTypes = map[ProductType][]vangogh_images.ImageType{
-	StoreProducts:    {vangogh_images.Image, vangogh_images.Screenshots},
+	StoreProducts: {
+		vangogh_images.Image,
+		vangogh_images.Screenshots,
+		vangogh_images.Video,
+	},
 	AccountProducts:  {vangogh_images.Image},
 	WishlistProducts: {vangogh_images.Image},
 	Details:          {vangogh_images.Background},
@@ -11,6 +15,7 @@ var supportedImageTypes = map[ProductType][]vangogh_images.ImageType{
 		vangogh_images.Icon,
 		vangogh_images.Background,
 		vangogh_images.Screenshots,
+		vangogh_images.Video,
 	},
 	ApiProductsV2: {
 		vangogh_images.Image,
@@ -20,6 +25,7 @@ var supportedImageTypes = map[ProductType][]vangogh_images.ImageType{
 		vangogh_images.Background,
 		vangogh_images.GalaxyBackground,
 		vangogh_images.Screenshots,
+		vangogh_images.Video,
 	},
 }
 
